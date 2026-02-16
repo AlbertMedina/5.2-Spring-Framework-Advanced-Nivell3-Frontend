86e956241acd2142d1e564ce0a168da7cb086ea6
Albert Medina <albertmecu@gmail.com>
2026-02-16 13:05:00 +0100

    feat: started defining structure + starting implementing auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthProvider } from "./services/auth.context";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Nav from "./components/core/Nav";
+import Footer from "./components/core/Footer";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import AdminHome from "./pages/admin/AdminHome";
+import UserHome from "./pages/userUserHome";
+
+import AuthContext from "./services/auth.context";
+
+function AppRoutes() {
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
+    return (
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    );
+  }
+
+  if (user.role === "admin") {
+    return (
+      <Routes>
+        <Route path="/" element={<AdminHome />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<UserHome />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Nav />
+        <AppRoutes />
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}

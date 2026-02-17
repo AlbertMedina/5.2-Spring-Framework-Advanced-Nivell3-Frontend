82a4abf34b6070fc85cfc78e1cf4de8dcc655f56
Albert Medina <albertmecu@gmail.com>
2026-02-17 22:02:40 +0100

    feat: added missing routes to user app

--- src/app/UserApp.jsx
+++ src/app/UserApp.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import UserHome from "../pages/user/UserHome";
+import Movies from "../pages/Movies";
+import MovieDetails from "../pages/MovieDetails";
 
 export default function UserApp() {
   return (
     <Routes>
       <Route path="/" element={<UserHome />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/movies/:movieId" element={<MovieDetails />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
